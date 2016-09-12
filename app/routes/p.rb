ee414188e6b032f9c3d11f3f229e4f3fdcf436af
Chris Elsworth <chris@cae.me.uk>
2016-09-12 01:17:39 +0100

    remove cache_control and fix last_modified for non-existant pages

--- app/routes/p.rb
+++ app/routes/p.rb
@@ -26,8 +26,7 @@ class Nabu
 			# it so they can view it. See how that logic goes..
 
 			r.is do
-				response.cache_control public: true, max_age: 86400
-				r.last_modified @pv.created_at
+				r.last_modified @pv&.created_at
 
 				# caching only active for guests; not admin
 				# using r.path as the cache key is for ngx_http_memcached_module
